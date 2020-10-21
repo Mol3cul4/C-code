d50c3b38ef4efb2f56d910a86806957ab23ca234
Mol3cul4 <44534742+Mol3cul4@users.noreply.github.com>
2020-10-21 19:03:53 -0300

    Add files via upload
    Chapter-1 (Luís Damas)

---
+++ prog0105.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main(void){
+	printf("Hoje está um \"LINDO\" dia!!!\n");
+}
---
+++ prog0107.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+	printf("Aqui vai um Apito\a\n");  /*Sinal Sonoro - \a */
+	printf("Aqui vai um Apito\7\n");
+}
