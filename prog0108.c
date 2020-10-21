0a4ac9be3ab2d455d6db699fbc25a7274b5c8b0f
Mol3cul4 <44534742+Mol3cul4@users.noreply.github.com>
2020-10-21 19:18:37 -0300

    Delete prog0108.c

--- prog0108.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	printf("Programa que apresenta os caracteres especiais\n\n");
-	printf("\\n\t-\t<ENTER>\n");
-	printf("\\\\\t-\t\\\n");
-	printf("\\t\t-\t<TAB>\n");
-	printf("%%%%\t-\t%%\n");
-}
